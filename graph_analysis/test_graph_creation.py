3a033aad316b4ff861ec97b16b62e72d20c63498
Shane Connelly <Shane.Connelly@gtri.gatech.edu>
2018-07-10 16:01:15 -0400

    Created a Test file for the graph_creation.py file and implemented the test_create_vertex_objects function as the test_vertex_to_dict() function since as of this commit they are functionally the same.

---
+++ graph_analysis/test_graph_creation.py
@@ -0,0 +1,50 @@
+import unittest
+import pandas as pd
+import networkx as nx
+
+from utils import create_vertex_objects
+from graph_objects import Vertex
+
+
+class TestGraphCreation(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_vertex_to_dict(self):
+        # This also tests the Vertex.to_dict() method in a round about way
+        data_dict = {'Component': ['Car', 'engine'],
+                     'Position': ['engine', 'Car'],
+                     'edge type': ['owner', 'type']}
+        test_graph_df = pd.DataFrame(data=data_dict)
+        Test_Graph = nx.DiGraph()
+        Temp_Graph = nx.DiGraph()
+        Temp_Graph = nx.from_pandas_edgelist(
+            df=test_graph_df, source='Component',
+            target='Position', edge_attr='edge type',
+            create_using=Temp_Graph)
+        edge_label_dict = {'edge type': 'owner'}
+        Test_Graph.add_nodes_from(Temp_Graph)
+        Test_Graph.add_edge('Car', 'engine', edge_attribute='owner')
+        Test_Graph.add_edge('engine', 'Car',
+                            edge_attribute='type')
+
+        verticies = create_vertex_objects(
+            df=test_graph_df, graph=Test_Graph)
+
+        vertex_1_dict = {'name': 'Car',
+                         'node types': {'Component', 'Position'},
+                         'successors': {'engine': {'edge_attribute': 'owner'}},
+                         'predecessors': {'engine':
+                                          {'edge_attribute': 'owner'}}}
+        vertex_2_dict = {'name': 'engine',
+                         'node types': {'Component', 'Position'},
+                         'successors': {'Car': {'edge_attribute': 'type'}},
+                         'predecessors': {'Car': {'edge_attribute': 'type'}}}
+        vertex_dicts = [vertex_1_dict, vertex_2_dict]
+
+        for index, vertex in enumerate(verticies):
+            self.assertDictEqual(vertex_dicts[index], vertex.to_dict())
+
+    def tearDown(self):
+        pass
