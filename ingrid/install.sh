93e0e0d2b27d53c3f9f06af8792e1645b9badb4e
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-05-28 18:35:20 -0400

    better error handling
    on macos -- conda activate does not work in script without the source ....conda.sh (which can be in one of 2 place)

--- ingrid/install.sh
+++ ingrid/install.sh
@@ -1,3 +1,23 @@
+#!/bin/bash
+
 conda create -y -n model_processing python=3.6
-conda activate model_processing || source activate model_processing
+conda activate model_processing
+if [ "$?" != 0 ]; then
+  FILE=~/anaconda3/etc/profile.d/conda.sh
+  if [ ! -f "$FILE" ]; then
+    FILE=~/miniconda3/etc/profile.d/conda.sh
+    if [ ! -f "$FILE" ]; then
+      echo "Failure executing conda activate. see recommendation above. Exiting"
+      exit
+    fi
+  fi
+  source $FILE
+  conda activate model_processing
+  if [ "$?" != 0 ]; then
+    echo "Failure executing conda activate. see recommendation above. Exiting"
+    exit
+  else
+    echo "Successfully activated model_processing environment"
+  fi
+fi
 python -m pip install -e . --ignore-installed
