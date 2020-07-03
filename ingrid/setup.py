f5aa66f2bbcff5285e1ecd972035b4995bc444d0
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-07-03 12:54:23 -0400

    Updated copyright date to 2020

--- ingrid/setup.py
+++ ingrid/setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2018 by the Georgia Tech Research Institute (GTRI)
+Copyright (C) 2020 by the Georgia Tech Research Institute (GTRI)
 This software may be modified and distributed under the terms of
 the BSD 3-Clause license. See the LICENSE file for details.
 """
