0b0e4963463d63d468c668ef3e09590b87f59774
Shane T Connelly <Shane.Connelly@gtri.gatech.edu>
2020-08-25 11:07:22 -0400

    optional args for each workflow.

--- ingrid/src/model_processing/gui.py
+++ ingrid/src/model_processing/gui.py
@@ -16,10 +16,8 @@ from gooey import Gooey, GooeyParser
 @Gooey(
     program_name="Rapid Modeling Tools: Ingrid",
     default_size=(710, 700),
-    tabbed_groups=True,
-    navigation="TABBED",
     program_description="GUI for Ingrid interactions",
-    use_cmd_args=True,
+    # use_cmd_args=True,
     optional_cols=2,
 )
 def main():
@@ -31,46 +29,23 @@ def main():
     # setup file.
 
     print("Welcome to the GUI for Rapid Modeling Tools: Ingrid")
+    settings_msg = (
+        "Welcome to the GUI for Rapid Modeling Tools: Ingrid"
+    )
 
-    parser = GooeyParser()
+    parser = GooeyParser(description=settings_msg)
 
     # fh_group = parser.add_argument_group("File Handling")
+    subs = parser.add_subparsers(help="commands", dest="commands")
 
-    pattern_up_group = parser.add_argument_group("Pattern Upload")
-    pattern_up_group.add_argument(
-        "custom_pattern",
-        help="Choose custom pattern file(s)",
-        metavar="Custom Pattern(s)",
+    create_parser = subs.add_parser(
+        "create",
+        help="Transform patterned input Excel data into Cameo API JSON",
+    )
+    create_parser.add_argument(
+        "Input",
         widget="MultiFileChooser",
-        default="",
-        gooey_options={
-            "wildcard": "JSON (*.json)|*.json",
-            "message": "Select your JSON pattern file(s)",
-            "default_file": "",
-        },
-    )
-    pattern_up_group.add_argument(
-        "custom_pattern_dir",
-        help="Upload an entire directory of custom JSON pattern file(s)",
-        metavar="Custom Pattern(s) Directory",
-        widget="DirChooser",
-        gooey_options={
-            "wildcard": "JSON (*.json)|*.json",
-            "message": "Select a directory containing JSON pattern file(s)",
-            "default_dir": "",
-        },
-    )
-    # parser.set_defaults(custom_pattern="", custom_pattern_dir="")
-
-    # multi-select widget for patterns with write in box for custom uploads
-    # check-box to store true if user applying multiple patterns to single
-    # data sheet.
-    create_group = parser.add_argument_group("Create")
-    create_group.add_argument(
-        "cr_file",
-        # action="Select the Create Excel file(s)",
         help="Choose File(s) to Create Model JSON From",
-        widget="MultiFileChooser",
         metavar="Create File(s)",
         gooey_options={
             "wildcard": "Xlsx (*.xlsx)|*.xlsx",
@@ -78,53 +53,136 @@ def main():
             "default_file": "",
         },
     )
-    create_group.add_argument(
-        "cr_dir",
-        help="Pick a directory of create files. Dir only contains RMT Excels",
-        metavar="Directory of Create Excel File(s)",
-        widget="DirChooser",
+    create_parser.add_argument(
+        "--pattern",
+        widget="MultiFileChooser",
+        help="Choose Custom Pattern File(s)",
+        metavar="Custom Pattern File(s)",
         gooey_options={
-            "wildcard": "Xlsx (*.xlsx)|*.xlsx",
-            "message": "Pick a directory contining input Excel file(s)",
+            "wildcard": "JSON (*.json)|*.json",
+            "message": "Select your JSON pattern file(s)",
+            "default_file": "",
+        },
+    )
+    create_parser.add_argument(
+        "--output",
+        widget="DirChooser",
+        help="Choose the Output Directory",
+        metavar="Desired Output Directory",
+        gooey_options={
+            "message": "Identify the desired output directory",
             "default_dir": "",
         },
     )
-    create_group.add_argument(
-        "cr_out_dir",
-        # action="",
-        help="Pick the output directory",
-        metavar="Output Directory",
+    create_parser.add_argument(
+        "--patterns",
+        type=list,
+        metavar="Comma Separated Pattern List",
+        help=(
+            "In a comma separated list (without spaces) identify the "
+            "desired patterns in order of application. Required for input "
+            "Excel files with multiple patterns in a single data sheet. "
+            "Example Input: Composition,PropertyRedef,SystemParts"
+        )
+    )
+
+    compare_parser = subs.add_parser(
+        "compare",
+        help="Compare two Excel Model versions"
+    )
+    compare_parser.add_argument(
+        "Original",
+        widget="FileChooser",
+        help="Upload the Original Excel Model File",
+        metavar="Create File",
+        gooey_options={
+            "wildcard": "Xlsx (*.xlsx)|*.xlsx",
+            "message": "Pick Excel File(s)",
+            "default_file": "",
+        },
+    )
+    compare_parser.add_argument(
+        "Changed",
+        widget="MultiFileChooser",
+        help="Choose File(s) to Compare to the Original Model",
+        metavar="Changed File(s)",
+        gooey_options={
+            "wildcard": "Xlsx (*.xlsx)|*.xlsx",
+            "message": "Pick Excel File(s)",
+            "default_file": "",
+        },
+    )
+    compare_parser.add_argument(
+        "--pattern",
+        widget="MultiFileChooser",
+        help="Choose Custom Pattern File(s)",
+        metavar="Custom Pattern File(s)",
+        gooey_options={
+            "wildcard": "JSON (*.json)|*.json",
+            "message": "Select your JSON pattern file(s)",
+            "default_file": "",
+        },
+    )
+    compare_parser.add_argument(
+        "--output",
         widget="DirChooser",
+        help="Choose the Output Directory",
+        metavar="Desired Output Directory",
         gooey_options={
             "message": "Identify the desired output directory",
             "default_dir": "",
         },
     )
 
-    compare_group = parser.add_argument_group("Compare")
-    # TODO: Pattern picker
-    compare_group.add_argument(
-        "og_file",
-        help="Upload the original Excel file",
+    layer_parser = subs.add_parser(
+        "pattern_layering",
+        help="Create the first pattern and comapre subsequent patterns"
+    )
+    layer_parser.add_argument(
+        "Data",
         widget="FileChooser",
+        help=(
+            "Upload the Data file which should have one sheet containing "
+            "all of the columns required for each pattern desired."
+        ),
+        metavar="Create File",
         gooey_options={
             "wildcard": "Xlsx (*.xlsx)|*.xlsx",
-            "message": "Pick Excel File",
+            "message": "Pick Excel File(s)",
             "default_file": "",
         },
     )
-    compare_group.add_argument(
-        "ch_file",
-        help="Choose file(s) to compare against the original",
+    layer_parser.add_argument(
+        "patterns",
+        type=list,
+        metavar="Comma Separated Pattern List",
+        help=(
+            "In a comma separated list (without spaces) identify the "
+            "desired patterns in order of application. Required for input "
+            "Excel files with multiple patterns in a single data sheet. "
+            "Example Input: Composition,PropertyRedef,SystemParts"
+        )
+    )
+    layer_parser.add_argument(
+        "--pattern",
         widget="MultiFileChooser",
+        help="Choose Custom Pattern File(s)",
+        metavar="Custom Pattern File(s)",
+        gooey_options={
+            "wildcard": "JSON (*.json)|*.json",
+            "message": "Select your JSON pattern file(s)",
+            "default_file": "",
+        },
     )
-    compare_group.add_argument(
-        "cmp_out_dir", help="Pick the output directory", widget="DirChooser",
-    )
-
-    layering_group = parser.add_argument_group("Pattern Layering")
-    layering_group.add_argument(
-        "--opt3", action="store_true", help="Option Three"
+    layer_parser.add_argument(
+        "--output",
+        widget="DirChooser",
+        help="Choose the Output Directory",
+        metavar="Desired Output Directory",
+        gooey_options={
+            "message": "Identify the desired output directory",
+            "default_dir": "",
+        },
     )
 
     # diff_resolver_group = parser.add_argument_group("Difference Resolver")
