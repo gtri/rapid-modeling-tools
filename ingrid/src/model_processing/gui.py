e4ed71e494d7c67230ba4d032103917f81bfb94b
Shane T Connelly <Shane.Connelly@gtri.gatech.edu>
2020-08-28 12:57:47 -0400

    additional command options for gui.

--- ingrid/src/model_processing/gui.py
+++ ingrid/src/model_processing/gui.py
@@ -6,18 +6,16 @@ the BSD 3-Clause license. See the LICENSE file for details.
 
 import argparse
 
-from .commands import compare_md_model, create_md_model
-from ._version import __version__
+from model_processing.commands import compare_md_model, create_md_model
+# from ._version import __version__
 
 from gooey import Gooey, GooeyParser
 
 
-# TODO: Menu items
 @Gooey(
     program_name="Rapid Modeling Tools: Ingrid",
     default_size=(710, 700),
     program_description="GUI for Ingrid interactions",
-    # use_cmd_args=True,
     optional_cols=2,
 )
 def main():
@@ -35,7 +33,6 @@ def main():
 
     parser = GooeyParser(description=settings_msg)
 
-    # fh_group = parser.add_argument_group("File Handling")
     subs = parser.add_subparsers(help="commands", dest="commands")
 
     create_parser = subs.add_parser(
@@ -45,7 +42,8 @@ def main():
     create_parser.add_argument(
         "Input",
         widget="MultiFileChooser",
-        help="Choose File(s) to Create Model JSON From",
+        nargs="*",
+        help="Choose File(s) for the CREATE command",
         metavar="Create File(s)",
         gooey_options={
             "wildcard": "Xlsx (*.xlsx)|*.xlsx",
@@ -74,17 +72,17 @@ def main():
             "default_dir": "",
         },
     )
-    create_parser.add_argument(
-        "--patterns",
-        type=list,
-        metavar="Comma Separated Pattern List",
-        help=(
-            "In a comma separated list (without spaces) identify the "
-            "desired patterns in order of application. Required for input "
-            "Excel files with multiple patterns in a single data sheet. "
-            "Example Input: Composition,PropertyRedef,SystemParts"
-        )
-    )
+    # create_parser.add_argument(
+    #     "--patterns",
+    #     type=str,
+    #     metavar="Comma Separated Pattern List",
+    #     help=(
+    #         "In a comma separated list (without spaces) identify the "
+    #         "desired patterns in order of application. Required for input "
+    #         "Excel files with multiple patterns in a single data sheet. "
+    #         "Example Input: Composition,PropertyRedef,SystemParts"
+    #     )
+    # )
 
     compare_parser = subs.add_parser(
         "compare",
@@ -94,7 +92,7 @@ def main():
         "Original",
         widget="FileChooser",
         help="Upload the Original Excel Model File",
-        metavar="Create File",
+        metavar="Original File: Excel representation of current model state",
         gooey_options={
             "wildcard": "Xlsx (*.xlsx)|*.xlsx",
             "message": "Pick Excel File(s)",
@@ -104,6 +102,7 @@ def main():
     compare_parser.add_argument(
         "Changed",
         widget="MultiFileChooser",
+        nargs="*",
         help="Choose File(s) to Compare to the Original Model",
         metavar="Changed File(s)",
         gooey_options={
@@ -115,6 +114,7 @@ def main():
     compare_parser.add_argument(
         "--pattern",
         widget="MultiFileChooser",
+        nargs="*",
         help="Choose Custom Pattern File(s)",
         metavar="Custom Pattern File(s)",
         gooey_options={
@@ -134,56 +134,56 @@ def main():
         },
     )
 
-    layer_parser = subs.add_parser(
-        "pattern_layering",
-        help="Create the first pattern and comapre subsequent patterns"
-    )
-    layer_parser.add_argument(
-        "Data",
-        widget="FileChooser",
-        help=(
-            "Upload the Data file which should have one sheet containing "
-            "all of the columns required for each pattern desired."
-        ),
-        metavar="Create File",
-        gooey_options={
-            "wildcard": "Xlsx (*.xlsx)|*.xlsx",
-            "message": "Pick Excel File(s)",
-            "default_file": "",
-        },
-    )
-    layer_parser.add_argument(
-        "patterns",
-        type=list,
-        metavar="Comma Separated Pattern List",
-        help=(
-            "In a comma separated list (without spaces) identify the "
-            "desired patterns in order of application. Required for input "
-            "Excel files with multiple patterns in a single data sheet. "
-            "Example Input: Composition,PropertyRedef,SystemParts"
-        )
-    )
-    layer_parser.add_argument(
-        "--pattern",
-        widget="MultiFileChooser",
-        help="Choose Custom Pattern File(s)",
-        metavar="Custom Pattern File(s)",
-        gooey_options={
-            "wildcard": "JSON (*.json)|*.json",
-            "message": "Select your JSON pattern file(s)",
-            "default_file": "",
-        },
-    )
-    layer_parser.add_argument(
-        "--output",
-        widget="DirChooser",
-        help="Choose the Output Directory",
-        metavar="Desired Output Directory",
-        gooey_options={
-            "message": "Identify the desired output directory",
-            "default_dir": "",
-        },
-    )
+    # layer_parser = subs.add_parser(
+    #     "pattern_layering",
+    #     help="Create the first pattern and comapre subsequent patterns"
+    # )
+    # layer_parser.add_argument(
+    #     "layering_input",
+    #     widget="FileChooser",
+    #     help=(
+    #         "Upload the Data file which should have one sheet containing "
+    #         "all of the columns required for each pattern desired."
+    #     ),
+    #     metavar="Pattern Layering File",
+    #     gooey_options={
+    #         "wildcard": "Xlsx (*.xlsx)|*.xlsx",
+    #         "message": "Pick Excel File(s)",
+    #         "default_file": "",
+    #     },
+    # )
+    # layer_parser.add_argument(
+    #     "patterns",
+    #     type=str,
+    #     metavar="Comma Separated Pattern List",
+    #     help=(
+    #         "In a comma separated list (without spaces) identify the "
+    #         "desired patterns in order of application. Required for input "
+    #         "Excel files with multiple patterns in a single data sheet. "
+    #         "Example Input: Composition,PropertyRedef,SystemParts"
+    #     )
+    # )
+    # layer_parser.add_argument(
+    #     "--pattern",
+    #     widget="MultiFileChooser",
+    #     help="Choose Custom Pattern File(s)",
+    #     metavar="Custom Pattern File(s)",
+    #     gooey_options={
+    #         "wildcard": "JSON (*.json)|*.json",
+    #         "message": "Select your JSON pattern file(s)",
+    #         "default_file": "",
+    #     },
+    # )
+    # layer_parser.add_argument(
+    #     "--output",
+    #     widget="DirChooser",
+    #     help="Choose the Output Directory",
+    #     metavar="Desired Output Directory",
+    #     gooey_options={
+    #         "message": "Identify the desired output directory",
+    #         "default_dir": "",
+    #     },
+    # )
 
     # diff_resolver_group = parser.add_argument_group("Difference Resolver")
     # Looks like a wx app can handle this.
@@ -191,17 +191,26 @@ def main():
     # I think we need Python state access so the other option is to run the
     # comparison again, assume same change issues
 
-    args = parser.parse_args()
-    print(args)
-    return args
-    # return parser.parse_args()
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = main()
-    print(args)
-    assert False
-    if args.cr_file or args.cr_dir:
-        inputs = [args.cr_file]
-        inputs.extend(args.cr_dir)
-        create_md_model(inputs, None, args.cr_out_dir)
+    if args.commands == "create":
+        # if args.patterns:
+        #     if len(args.Input) == 1 and len(patterns) > 1:
+        #         pass
+        #     elif len(args.patterns) == len(args.Input):
+        #         create_from_pattern(
+        #             args.Input,
+        #             args.patterns,
+        #             new_pats=args.pattern,
+        #             output_path=args.output
+        #         )
+        # else:
+        #     create_md_model(args.Input, args.pattern, args.output)
+        create_md_model(args.Input, args.pattern, args.output)
+    if args.commands == "compare":
+        inputs = [args.Original]
+        inputs.extend(args.Changed)
+        compare_md_model(inputs, args.pattern, args.output)
