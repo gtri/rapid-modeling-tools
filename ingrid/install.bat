e572144b711eceee32db76ebadb9945415d6ada5
Shane T Connelly <Shane.Connelly@gtri.gatech.edu>
2020-09-11 13:34:50 -0400

    added build spec and application build call to install scripts.

--- ingrid/install.bat
+++ ingrid/install.bat
@@ -2,3 +2,4 @@ call conda create -y -n model-processing python=3.6
 call activate model-processing && python -V
 call python -m pip install --upgrade pip
 call pip install -r requirements-install.txt
+call pyinstaller build.spec
--- ingrid/setup.py
+++ ingrid/setup.py
@@ -34,7 +34,6 @@ setup(
     entry_points={
         "console_scripts": [
             "model-processing = model_processing.cli:main",
-            "model-processing-gui = model_processing.gui:main",
         ],
     },
     classifiers=[
